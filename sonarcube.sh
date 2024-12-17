f3b7568c868461e3ff77595225400d823d851c5e
Nequow <74051411+Nequow@users.noreply.github.com>
2024-12-17 16:08:35 +0100

    add sonarcube

---
+++ sonarcube.sh
@@ -0,0 +1,4 @@
+mvn clean verify sonar:sonar \
+  -Dsonar.projectKey=708e84660adc80f915c2e7aff15a6e47f3824cb2c38a7e1b40d5372bf7a068f6 \
+  -Dsonar.host.url=http://localhost:9001 \
+  -Dsonar.login=sqp_242ee134430c2f9e3c43b5c7982cca39bb8dd082
